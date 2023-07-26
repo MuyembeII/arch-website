34381e30ed3b0eadde0f1abff75bd7fbf70ce471
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-07-26 15:04:53 +0200

    implement responsiveness

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/js/app.js')
     <!-- Styles -->
-    <style>
 
-    </style>
 
     <style>
         body {
@@ -29,17 +29,16 @@
     <div class=" h-screen">
         <div class="flex items-center p-4 justify-between  md:p-8">
             <h1 class="">ARCH Projects</h1>
-            <input type="checkbox" id="check" hidden>
-            <label for="check" class="float-right  text-3xl leading-[80px] lg:hidden">
+            <button class=" hamburger float-right  text-3xl  lg:hidden">
                 <i class="fa-solid fa-bars"></i>
-            </label>
+            </button>
         </div>
 
-        <div class=" dark:bg-gray-900 h-2/6 lg:h-14  ">
-            <ul class=" flex flex-col lg:flex-row justify-around lg:items-center h-full px-8 leading-[80px]  ">
-                <li class="text-sm text-gray-700 dark:text-gray-500 "> <a href="{{ url('/projects') }}">Projects</a></li>
-                <li class="text-sm text-gray-700 dark:text-gray-500 "> <a href="{{ url('/contact') }}"> Contact Us</a>
-                <li class="text-sm text-gray-700 dark:text-gray-500 "><a href="{{ url('/about') }}">About Us</a></li>
+        <div class="menu dark:bg-gray-900  lg:h-14  ">
+            <ul class="flex lg:flex-row justify-around lg:items-center dark:bg-gray-900 text-white h-full px-8   ">
+                <li class="text-sm"> <a href="{{ url('/projects') }}">Projects</a></li>
+                <li class="text-sm"> <a href="{{ url('/contact') }}"> Contact Us</a>
+                <li class="text-sm"><a href="{{ url('/about') }}">About Us</a></li>
             </ul>
         </div>
 
