90d115eefe04fc054423e9e52e28a43660d612a2
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-07-27 13:59:46 +0200

    Responsive Navbar (#1)
    * implement desktop navbar
    
    * implement responsiveness

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
-import './bootstrap';
+import "./bootstrap";
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu ul");
+const navLinks = document.querySelectorAll(".menu a");
+
+allEventListeners();
+
+function allEventListeners() {
+    if (hamburger) {
+        hamburger.addEventListener("click", togglerClick);
+    }
+    if (navLinks) {
+        navLinks.forEach((elem) =>
+            elem.addEventListener("click", navLinkClick)
+        );
+    }
+}
+
+function togglerClick() {
+    hamburger.classList.toggle("hamburger-open");
+    navMenu.classList.toggle("open");
+    console.log("hamburger");
+}
+
+function navLinkClick() {
+    if (navMenu.classList.contains("open")) {
+        hamburger.click();
+    }
+}
